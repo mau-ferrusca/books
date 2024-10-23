e0b6db90eb5b60869c8a8c59a0d30f15e424d7c4
MauFerrusca <mferrusca@gmail.com>
2024-10-23 15:31:41 -0600

    EHs collapsed

--- src/components/BookShow.js
+++ src/components/BookShow.js
@@ -2,21 +2,22 @@ import { useState } from "react";
 import BookEdit from "./BookEdit";
 
 function BookShow({ book, onDelete, onEdit }) {
-  const [title, setTitle] = useState(book.title);
   const [showEdit, setShowEdit] = useState(false);
-
   const handleDeleteClick = () => {
     onDelete(book.id);
   };
-
   const handleEditClick = () => {
     setShowEdit(!showEdit);
   };
+  const handleSubmit = (id, newTitle) => {
+    setShowEdit(false);
+    onEdit(id, newTitle);
+  }
 
   let content = <h3>{book.title}</h3>
 
   if (showEdit) {
-    content = <BookEdit book={book} onEdit={onEdit} />
+    content = <BookEdit book={book} onSubmit={handleSubmit} />
   };
 
   return (
